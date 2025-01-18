503214a12f65f7efabd6414cdd641ce89dc91d91
Prathi <prathiba2654@gmail.com>
2025-01-18 22:31:10 +0530

    new change
    habit tracker and home page , little changes in app.js and index js too

--- src/components/HabitTracker.js
+++ src/components/HabitTracker.js
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import Navbar from './Navbar';
+import './habit.css';
 function HabitTracker() {
   return (
     <div className="habit-tracker">
+      
+      <div className="habit-tracker">
       <h2>Habit Tracker</h2>
       <p>Track and maintain your daily habits.</p>
-    </div>
+      <div className="habits">
+        <div className="habit">
+          <h3>Exercise</h3>
+          <p>30 minutes</p>
+          <button>+</button>
+          <button>-</button>
+        </div>
+        <div className="habit">
+          <h3>Read</h3>
+          <p>30 minutes</p>
+          <button>+</button>
+          <button>-</button>
+        </div>
+        <div className="habit">
+          <h3>Meditate</h3>
+          <p>10 minutes</p>
+          <button>+</button>
+          <button>-</button>
+        </div>
+        <div className="habit">
+          <h3>Journal</h3>
+          <p>10 minutes</p>
+          <button>+</button>
+          <button>-</button>
+        </div>
+      </div>
+      </div>
+      </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,6 @@ import React, { useRef } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
 
-// Import sound effect
-//import clickSound from "../assets/sounds/click.mp3";
-
 function Navbar() {
   const audioRef = useRef(new Audio("pop.mp3"));
 
@@ -47,14 +44,14 @@ function Navbar() {
         </li>
         <li onClick={handleClick}>
           <Link to="/community-resources">
-            <img src="com.png" alt="Community Resources" />
-            <span className="text">Community Resources</span>
+            <img src="community.png" alt="Community Resources" />
+            <span className="text">Community</span>
           </Link>
         </li>
         <li onClick={handleClick}>
           <Link to="/transformation-visualizer">
             <img src="graph.webp" alt="Transformation Visualizer" />
-            <span className="text">Transformation Visualizer</span>
+            <span className="text">Visualizer</span>
           </Link>
         </li>
       </ul>
